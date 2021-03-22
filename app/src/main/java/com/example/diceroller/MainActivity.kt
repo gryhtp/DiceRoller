e93d4f40942664c01c12e2979ccd3cdea9c2e47b
gryhtp <hatipguray@gmail.com>
2021-03-22 12:02:14 +0100

    Lab1

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -18,15 +19,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = (1..6).random()
-        //Toast.makeText(this, "button clicked",
-        //      Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
     }
 
     private fun resetDice() {
         val zeroInt = 0
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = zeroInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = R.drawable.empty_dice
+        diceImage.setImageResource(drawableResource)
     }
 }
